53839c5aa976820ba8c8d0ffc2fce18f3c08a199
guptakaran0720 <guptakaran0720@gmail.com>
2026-08-19 00:12:03 +0530

    cors issue

--- backend/utils/socket.js
+++ backend/utils/socket.js
@@ -5,7 +5,7 @@ module.exports = {
     const { Server } = require('socket.io');
     io = new Server(server, {
       cors: {
-        origin: ['http://aarogya-vert.vercel.app', 'https://aarogya-vert.vercel.app', 'http://localhost:5173', 'http://localhost:5174'],
+        origin: ['http://aarogya-vert.vercel.app', 'https://aarogya-vert.vercel.app', 'http://localhost:5173', 'http://localhost:5174', 'https://www.aarogyanitj.in', 'https://aarogyanitj.in'],
         methods: ['GET', 'POST'],
         credentials: true
       }
